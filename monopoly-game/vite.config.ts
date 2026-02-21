c9a5b38dfd3ab7782ed8415b360dd23c84480613
MayyDayy99 <pr.nemes@gmail.com>
2026-02-21 17:06:52 +0100

    page_2

--- monopoly-game/vite.config.ts
+++ monopoly-game/vite.config.ts
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: './', // Szigorú relatív útvonalak GitHub Pages-hez
+  base: '/monopoly/', // GitHub Pages Subdomain /monopoly/ almappához
 })
